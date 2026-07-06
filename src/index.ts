c9ccc9fd4adf539ef97e335983a90a839e1dfbeb
Phạm Ngọc Quốc Khánh <pnqkhanh2924@gmail.com>
2026-07-06 19:54:26 +0700

    chore: export border style

--- src/index.ts
+++ src/index.ts
@@ -1,57 +1,55 @@
-export * from './components/base/Avatar';
-export { default as Avatar } from './components/base/Avatar';
+export * from "./components/base/Avatar";
+export { default as Avatar } from "./components/base/Avatar";
 
+export * from "./components/base/Badge";
+export { default as Badge } from "./components/base/Badge";
 
+export * from "./components/base/Button";
+export { default as Button } from "./components/base/Button";
 
-export * from './components/base/Badge';
-export { default as Badge } from './components/base/Badge';
+export * from "./components/base/Card";
+export { default as Card } from "./components/base/Card";
 
-export * from './components/base/Button';
-export { default as Button } from './components/base/Button';
+export * from "./components/base/Dialog";
+export { default as Dialog } from "./components/base/Dialog";
 
-export * from './components/base/Card';
-export { default as Card } from './components/base/Card';
+export * from "./components/base/Icon";
+export { default as Icon } from "./components/base/Icon";
 
-export * from './components/base/Dialog';
-export { default as Dialog } from './components/base/Dialog';
+export * from "./components/base/Image";
+export { default as Image } from "./components/base/Image";
 
-export * from './components/base/Icon';
-export { default as Icon } from './components/base/Icon';
+export * from "./components/base/InputView";
+export { default as InputView } from "./components/base/InputView";
 
-export * from './components/base/Image';
-export { default as Image } from './components/base/Image';
+export * from "./components/base/Progressbar";
 
-export * from './components/base/InputView';
-export { default as InputView } from './components/base/InputView';
+export * from "./components/base/Radio";
 
-export * from './components/base/Progressbar';
+export * from "./components/base/Slider";
+export { default as Slider } from "./components/base/Slider";
 
-export * from './components/base/Radio';
+export * from "./components/base/Switch";
+export { default as Switch } from "./components/base/Switch";
 
-export * from './components/base/Slider';
-export { default as Slider } from './components/base/Slider';
+export * from "./components/base/Text";
+export { default as Text } from "./components/base/Text";
 
-export * from './components/base/Switch';
-export { default as Switch } from './components/base/Switch';
+export * from "./components/base/TouchableOpacity";
+export { default as TouchableOpacity } from "./components/base/TouchableOpacity";
 
-export * from './components/base/Text';
-export { default as Text } from './components/base/Text';
-
-export * from './components/base/TouchableOpacity';
-export { default as TouchableOpacity } from './components/base/TouchableOpacity';
-
-export * from './components/base/View';
-export { default as View } from './components/base/View';
+export * from "./components/base/View";
+export { default as View } from "./components/base/View";
 
 // Provider
-export * from './provider/QKProvider';
+export * from "./provider/QKProvider";
 
 // Styles
-export { abs } from './styles/abs.style';
-export { baseStyle } from './styles/base.style';
-export { gapStyle } from './styles/gap.style';
-export { marginStyle } from './styles/margin.style';
-export { paddingStyle } from './styles/padding.style';
-export { radiusStyle } from './styles/radius.style';
-export * from './styles/typography.style';
-export { default as Typography } from './styles/typography.style';
+export { abs } from "./styles/abs.style";
+export { baseStyle } from "./styles/base.style";
+export { gapStyle } from "./styles/gap.style";
+export { marginStyle } from "./styles/margin.style";
+export { paddingStyle } from "./styles/padding.style";
+export { radiusStyle, borderStyle } from "./styles/radius.style";
+export * from "./styles/typography.style";
+export { default as Typography } from "./styles/typography.style";
